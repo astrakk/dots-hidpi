cbbae1dcfb7efe0c3d329a26b4d8b5dd83b7cc50
Jack <jackon223@gmail.com>
2017-09-04 21:38:34 +1000

    Changed behaviour of i3_workspaces script to output workspaces information as singular string

--- polybar/.config/polybar/scripts/i3_workspaces.sh
+++ polybar/.config/polybar/scripts/i3_workspaces.sh
@@ -6,15 +6,18 @@ workspaces_list=(1 2 3 4 5 6 7 8 9 10)
 workspaces_active="$(i3-msg -t get_workspaces | jq '.[] | .num')"
 workspace_visible="$(i3-msg -t get_outputs | jq '.[] | .current_workspace')"
 workspace_focused="$(i3-msg -t get_workspaces | jq '.[] | select(.focused).num')"
+workspaces_output=""
 
 for ws in ${workspaces_list[@]}; do
      if [[ $(echo "${workspace_focused}" | grep -w "$ws") ]]; then
-          printf '%%{F'$colour0D'} ■ %%{F-}'
+          workspaces_output+='%%{F'$colour0D'} ■ %%{F-}'
      elif [[ $(echo "${workspace_visible}" | grep -w "$ws") ]]; then
-          printf '%%{F'$colour0D'} □ %%{F-}'
+          workspaces_output+='%%{F'$colour0D'} □ %%{F-}'
      elif [[ $(echo "${workspaces_active}" | grep -w "$ws") ]]; then
-          printf '%%{F'$colour05'} ■ %%{F-}'
+          workspaces_output+='%%{F'$colour05'} ■ %%{F-}'
      else
-          printf '%%{F'$colour03'} ■ %%{F-}'
+          workspaces_output+='%%{F'$colour03'} ■ %%{F-}'
      fi
 done
+
+printf "$workspaces_output"
