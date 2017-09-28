7b53a3561dc405f8fc103060a47e045078a7c87e
Jack <jack@astrak.co>
2017-09-28 16:27:38 +1000

    Reverted custom polybar modules due to high cpu usage

--- polybar/.config/polybar/scripts/brightness.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# checking if device even has a backlight setting
-if [[ $(find /sys/class/backlight/* 2>/dev/null) ]]; then
-     echo -e "$(xbacklight -get)"
-fi
--- polybar/.config/polybar/scripts/window_title.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# output the current active window name, truncate it to 30 characters, and add an elipsis to the end if it's too long or print blank line if no active window found
-if [[ $(xdotool getactivewindow 2>/dev/null) ]]; then
-     xdotool getactivewindow getwindowname | sed -e 's/\(.*\)/\L\1/' | awk -v len=33 '{ if (length($0) > len) print substr($0, 1, len-3) "..."; else print; }'
-else
-     echo ""
-fi
