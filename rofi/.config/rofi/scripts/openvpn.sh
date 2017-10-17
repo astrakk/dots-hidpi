52ca3e50b25a62cc46c32d0895a84bbca3662e5e
Jack <jack@astrak.co>
2017-10-17 15:23:59 +1000

    Corrected capitalization in openvpn rofi script

--- rofi/.config/rofi/scripts/openvpn.sh
+++ rofi/.config/rofi/scripts/openvpn.sh
@@ -5,21 +5,21 @@ openvpn_config=~/.config/openvpn/config.ovpn
 
 # display different options depending on whether compton is running or not
 if [[ $(pgrep -x "openvpn" 2>/dev/null) ]]; then
-     options=( '1: Stop openvpn' )
-     rofi_width=310
+     options=( '1: Stop OpenVPN' )
+     rofi_width=274
      rofi_lines=1
 else
-     options=( '1: Start openvpn' )
-     rofi_width=330
+     options=( '1: Start OpenVPN' )
+     rofi_width=286
      rofi_lines=1
 fi
 
 select_option() {
      case $1 in
-          '1: Start openvpn' )
+          '1: Start OpenVPN' )
                pkexec openvpn $openvpn_config
                ;;
-          '1: Stop openvpn' )
+          '1: Stop OpenVPN' )
                pkexec killall -q openvpn
                ;;
      esac
